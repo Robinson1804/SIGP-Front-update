1522df34d5802bd6fc4b36c2e0fd84d45704fff7
Angella Trujillo <angijankylcar@gmail.com>
2025-10-22 20:01:53 +0000

    Al ingresr a  PROYECTOS PGD debe seguir o continuar en modulo PGd, no en

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -83,7 +83,7 @@ export default function AppLayout({
   let activeNavItemLabel;
   const pgdSubmodules = ['/oei-dashboard', '/ogd-dashboard', '/oegd-dashboard', '/ae-dashboard', '/poi-dashboard'];
   
-  if (pathname.startsWith('/poi')) {
+  if (pathname === '/poi') {
       activeNavItemLabel = 'POI';
   } else {
       const activeItem = navItems.find(item => item.href === pathname);
